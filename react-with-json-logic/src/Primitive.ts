3ebbc9af661fcaa61c05ed11e3271ce77135ef69
Janna Wieneke <janna.wieneke@posteo.de>
2020-04-12 13:46:43 +0200

    Fix isPrimitive (use indexOf instead of in)

--- react-with-json-logic/src/Primitive.ts
+++ react-with-json-logic/src/Primitive.ts
@@ -1,5 +1,5 @@
 export type Primitive = string | boolean | number;
 
 export const isPrimitive = (value: unknown): value is Primitive => {
-  return typeof value in ["string", "boolean", "number"];
+  return ["string", "boolean", "number"].indexOf(typeof value) > -1;
 };
