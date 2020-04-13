8f2918715af95d7825778850a23d4e8ff920b82c
Christoph Wersal <christoph.wersal@tngtech.com>
2020-04-13 13:48:00 +0200

    Add text test

--- react-with-json-logic/src/components/questionComponents/TextInput.tsx
+++ react-with-json-logic/src/components/questionComponents/TextInput.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextField, FormControl, FormLabel } from "@material-ui/core";
+import { FormControl, FormLabel, TextField } from "@material-ui/core";
 import { QuestionFormComponentProps } from "./QuestionFormComponent";
 
 export const TextInput: React.FC<QuestionFormComponentProps> = ({ currentQuestion, onChange }) => {
@@ -15,7 +15,7 @@ export const TextInput: React.FC<QuestionFormComponentProps> = ({ currentQuestio
   return (
     <FormControl component="fieldset">
       <FormLabel component="legend">{currentQuestion.text}</FormLabel>
-      <TextField id={currentQuestion.id} autoFocus={true} onChange={handleChange} />
+      <TextField id={currentQuestion.id} autoFocus={true} onChange={handleChange} data-testid={"TextInput"} />
     </FormControl>
   );
 };
