c47497b1d0c5ee8834a8e509e2a52a03b3542fa3
Janna Wieneke <janna.wieneke@posteo.de>
2020-04-18 16:08:42 +0200

    Generate title for referenced type definitons

--- ts-json-schema-generator/src/TypeFormatter/DefinitionTypeFormatter.ts
+++ ts-json-schema-generator/src/TypeFormatter/DefinitionTypeFormatter.ts
@@ -6,14 +6,19 @@ import { TypeFormatter } from "../TypeFormatter";
 import { uniqueArray } from "../Utils/uniqueArray";
 
 export class DefinitionTypeFormatter implements SubTypeFormatter {
-    public constructor(private childTypeFormatter: TypeFormatter, private encodeRefs: boolean) {}
+    public constructor(private childTypeFormatter: TypeFormatter, private encodeRefs: boolean) { }
 
     public supportsType(type: DefinitionType): boolean {
         return type instanceof DefinitionType;
     }
     public getDefinition(type: DefinitionType): Definition {
         const ref = type.getName();
-        return { $ref: `#/definitions/${this.encodeRefs ? encodeURIComponent(ref) : ref}` };
+        const definition: Definition = { $ref: `#/definitions/${this.encodeRefs ? encodeURIComponent(ref) : ref}` };
+        const elementType: any = type.getType();
+        if ((elementType != null) && (elementType.annotations != null) && (elementType.annotations.title != null)) {
+            definition.title = elementType.annotations.title;
+        }
+        return definition
     }
     public getChildren(type: DefinitionType): BaseType[] {
         return uniqueArray([type, ...this.childTypeFormatter.getChildren(type.getType())]);
