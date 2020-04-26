defb6e33bcfd9c3e81d41370cce3c250e3cc4a0a
Janna Wieneke <janna.wieneke@posteo.de>
2020-04-26 17:39:11 +0200

    Change width of questionnaire execution

--- react-with-json-logic/src/components/QuestionnaireExecution.tsx
+++ react-with-json-logic/src/components/QuestionnaireExecution.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Grid, Paper, Typography } from "@material-ui/core";
+import { Box, Button, Grid, Paper, Typography, makeStyles, createStyles } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import { Question, QuestionnaireEngine, Result } from "../logic/QuestionnaireEngine";
 import { ResultComponent } from "./ResultComponent";
@@ -12,6 +12,17 @@ type QuestionnaireExecutionProps = {
   currentQuestionnaire: Questionnaire;
 };
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      width: "100%",
+    },
+    paddingRight: {
+      paddingLeft: 12,
+    },
+  })
+);
+
 export const QuestionnaireExecution: React.FC<QuestionnaireExecutionProps> = ({
   isJsonInvalid,
   currentQuestionnaire,
@@ -20,6 +31,8 @@ export const QuestionnaireExecution: React.FC<QuestionnaireExecutionProps> = ({
   const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>(undefined);
   const [result, setResult] = useState<Result[] | undefined>(undefined);
   const [doRerender, setDoRerender] = useState(false);
+
+  const classes = useStyles();
 
   function restartQuestionnaire() {
     const engine = new QuestionnaireEngine(currentQuestionnaire);
@@ -54,24 +67,24 @@ export const QuestionnaireExecution: React.FC<QuestionnaireExecutionProps> = ({
   return doRerender ? (
     <></>
   ) : (
-    <>
-      <Grid item xs={9} className="grid-row">
+    <div className={classes.root}>
+      <Grid item xs={12} className={`${classes.paddingRight} grid-row`}>
         <Button onClick={restartQuestionnaire} variant="contained" color="secondary">
           Restart Questionnaire
         </Button>
       </Grid>
       {isJsonInvalid ? (
-        <Grid item xs={9} className="grid-row">
+        <Grid item xs={12} className={`${classes.paddingRight} grid-row`}>
           <Alert severity="warning">Cannot load questionnaire. JSON is invalid!</Alert>
         </Grid>
       ) : null}
-      <Grid item xs={9} className="grid-row">
+      <Grid item xs={12} className={`${classes.paddingRight} grid-row`}>
         {result === undefined && currentQuestion ? (
           <QuestionComponent currentQuestion={currentQuestion} handleNextClick={handleNextClick} />
         ) : null}
         {result !== undefined ? <ResultComponent result={result} /> : null}
       </Grid>
-      <Grid item xs={9} className="grid-row">
+      <Grid item xs={12} className={`${classes.paddingRight} grid-row`}>
         {questionnaireEngine ? (
           <>
             <Typography>Current internal state:</Typography>
@@ -83,6 +96,6 @@ export const QuestionnaireExecution: React.FC<QuestionnaireExecutionProps> = ({
           </>
         ) : null}
       </Grid>
-    </>
+    </div>
   );
 };
