b454ce60c037903de70f80a1ffbd9cfec558b665
Janna Wieneke <janna.wieneke@posteo.de>
2020-04-26 14:44:04 +0200

    Set color of secondary button for form editor

--- react-with-json-logic/src/components/questionnaireEditor/QuestionnaireFormEditor.tsx
+++ react-with-json-logic/src/components/questionnaireEditor/QuestionnaireFormEditor.tsx
@@ -89,6 +89,9 @@ export function QuestionnaireFormEditor(props: QuestionnaireFormEditorProps) {
     .rjsf .MuiGrid-item {
       padding: 0px 8px 0px 8px;
     }
+    .rjsf .MuiButton-textSecondary {
+      color: rgba(0, 0, 0, 0.87);
+    }
     `;
 
   useEffect(() => {
