aef5fac3ec469a51c74a23303f193262089251dc
Emanuel Joebstl <emanuel.joebstl@gmail.com>
2020-05-10 16:17:23 +0200

    Fix: Incorrect brace insertion when generating expressions.

--- covscript/test/endToEnd.spec.ts
+++ covscript/test/endToEnd.spec.ts
@@ -33,13 +33,18 @@ describe("Generator/Parser End-To-End Tests", () => {
     '1 > 0 and 7 - 2 % 3 == 1 or "Test" != Test'
   );
 
-  expectE2E("1 - 2 + 2", "1 - 2 + 2");
+  expectE2E('1 - (2 + 2)', '1 - (2 + 2)')
+  expectE2E('1 - 2 + 2', '1 - 2 + 2')
 
-  expectE2E("1 + 2 - 2", "1 + 2 - 2");
+  expectE2E('1 + (2 - 2)', '1 + 2 - 2', true)
+  expectE2E('1 + 2 - 2', '1 + 2 - 2')
 
-  expectE2E("1 / 2 * 2", "1 / 2 * 2");
+  expectE2E('1 / (2 * 2)', '1 / (2 * 2)')
+  expectE2E('1 / 2 * 2', '1 / 2 * 2')
 
-  expectE2E("1 * 2 / 2", "1 * 2 / 2");
+  expectE2E('1 * (2 / 2)', '1 * 2 / 2', true)
+  expectE2E('1 * 2 / 2', '1 * 2 / 2')
 
-  expectE2E("1 * 2 % 2", "1 * 2 % 2");
+  expectE2E('1 * (2 % 2)', '1 * 2 % 2', true)
+  expectE2E('1 * 2 % 2', '1 * 2 % 2')
 });
