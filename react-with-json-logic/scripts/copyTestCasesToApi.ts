51a045e3b9f388e4d7cc3e030e20e9c58177cc1b
Janna Wieneke <janna.wieneke@posteo.de>
2020-05-12 18:12:39 +0200

    Change script to create create questionnaires in api like folder structure

--- react-with-json-logic/scripts/copyTestCasesToApi.ts
+++ react-with-json-logic/scripts/copyTestCasesToApi.ts
@@ -3,12 +3,19 @@ import * as path from "path";
 
 const testCasePath = path.join(__dirname, "../src/test/testCases/");
 
-const apiBasePath = path.join(__dirname, "../public/api/");
+const questionnairesBasePath = path.join(__dirname, "../public/api/questionnaires/");
 
 const fileNames = fs.readdirSync(testCasePath);
 
 fileNames.forEach((fileName: string) => {
   const questionnaire = require(testCasePath + fileName);
-  const jsonPath = apiBasePath + questionnaire.default.id + ".json";
-  fs.writeFileSync(jsonPath, JSON.stringify(questionnaire.default, undefined, 2));
+  let path = `${questionnairesBasePath}${questionnaire.default.id}/`;
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+  path = `${questionnairesBasePath}${questionnaire.default.id}/${questionnaire.default.version}/`;
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+  fs.writeFileSync(`${path}${questionnaire.default.language}`, JSON.stringify(questionnaire.default, undefined, 2));
 });
