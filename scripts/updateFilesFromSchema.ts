b2457aea828c381cc7d643f4b126dbd26a6af9e2
Janna Wieneke <janna.wieneke@posteo.de>
2020-05-30 09:45:23 +0200

    Rename covquestions-app to covquestions-editor-app

--- scripts/updateFilesFromSchema.ts
+++ scripts/updateFilesFromSchema.ts
@@ -2,6 +2,6 @@ import * as fs from "fs";
 import { loadSchema } from "./loadSchema";
 
 fs.writeFileSync(
-  "./covquestions-app/src/schemas/questionnaire.json",
+  "./covquestions-editor-app/src/schemas/questionnaire.json",
   JSON.stringify(loadSchema(), null, 2)
 );
