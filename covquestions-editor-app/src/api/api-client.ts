291fbfb97ba8b2f5b9a821f3b56aeac0667ac998
Daniel Habenicht <daniel-habenicht@outlook.de>
2020-06-28 00:40:00 +0200

    use new endpoint

--- covquestions-editor-app/src/api/api-client.ts
+++ covquestions-editor-app/src/api/api-client.ts
@@ -2,11 +2,14 @@ import { ISOLanguage, Questionnaire } from "covquestions-js/models/Questionnaire
 import { QuestionnaireBaseData } from "../models/QuestionnairesList";
 
 const rootUrl = process.env.REACT_APP_API_URL || "";
+const urlSuffix = ".json";
 
 export async function getAllQuestionnaires(): Promise<QuestionnaireBaseData[]> {
   let url = rootUrl + "/questionnaires";
   if (process.env.NODE_ENV !== "production") {
     url += ".json";
+  }else{
+    url += urlSuffix
   }
   let response = await fetch(url);
   if (response.ok) {
@@ -20,7 +23,7 @@ export async function getQuestionnaireByIdVersionAndLanguage(
   version: number,
   language: ISOLanguage
 ): Promise<Questionnaire | undefined> {
-  const response = await fetch(`${rootUrl}/questionnaires/${id}/${version}/${language}`);
+  const response = await fetch(`${rootUrl}/questionnaires/${id}/${version}/${language}${urlSuffix}`);
   if (response.ok) {
     return await response.json();
   }
